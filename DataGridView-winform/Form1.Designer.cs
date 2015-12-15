84ecde2ee319073d5c6179254de8ad6032dd3785
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2015-12-15 17:49:57 +0800

    移除 bindingSource ，用自訂 DataSource 方式就好

--- DataGridView-winform/Form1.Designer.cs
+++ DataGridView-winform/Form1.Designer.cs
@@ -23,26 +23,37 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gvSample = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.ckMarried = new System.Windows.Forms.CheckBox();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gvbtnDel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gvName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gvGender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gvMarried = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gvBirthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSample)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // gvSample
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 147);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(555, 292);
-            this.dataGridView1.TabIndex = 0;
+            this.gvSample.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvSample.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gvbtnDel,
+            this.gvName,
+            this.gvGender,
+            this.gvMarried,
+            this.gvBirthday});
+            this.gvSample.Location = new System.Drawing.Point(12, 147);
+            this.gvSample.Name = "gvSample";
+            this.gvSample.RowTemplate.Height = 24;
+            this.gvSample.Size = new System.Drawing.Size(555, 292);
+            this.gvSample.TabIndex = 0;
             // 
             // button1
             // 
@@ -53,64 +64,98 @@
             this.button1.Text = "Add New";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(138, 22);
-            this.textBox1.TabIndex = 2;
+            this.txtName.Location = new System.Drawing.Point(120, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 22);
+            this.txtName.TabIndex = 2;
             // 
-            // comboBox1
+            // cbGender
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(120, 53);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(138, 20);
-            this.comboBox1.TabIndex = 3;
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Location = new System.Drawing.Point(120, 50);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(138, 20);
+            this.cbGender.TabIndex = 3;
             // 
-            // checkBox1
+            // ckMarried
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(120, 86);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(77, 16);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.ckMarried.AutoSize = true;
+            this.ckMarried.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ckMarried.Location = new System.Drawing.Point(120, 78);
+            this.ckMarried.Name = "ckMarried";
+            this.ckMarried.Size = new System.Drawing.Size(87, 24);
+            this.ckMarried.TabIndex = 4;
+            this.ckMarried.Text = "Married";
+            this.ckMarried.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // dtpBirthday
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(120, 108);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(138, 22);
-            this.dateTimePicker1.TabIndex = 5;
+            this.dtpBirthday.Location = new System.Drawing.Point(120, 115);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(138, 22);
+            this.dtpBirthday.TabIndex = 5;
+            this.dtpBirthday.Value = new System.DateTime(2015, 12, 15, 17, 11, 44, 0);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(28, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 6;
-            this.label1.Text = "TextBox";
+            this.label1.Text = "Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 56);
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(28, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.Size = new System.Drawing.Size(65, 20);
             this.label2.TabIndex = 7;
-            this.label2.Text = "ComboBox";
+            this.label2.Text = "Gender";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label4.Location = new System.Drawing.Point(28, 115);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 12);
+            this.label4.Size = new System.Drawing.Size(72, 20);
             this.label4.TabIndex = 9;
-            this.label4.Text = "DateTimePicker";
+            this.label4.Text = "Birthday";
+            // 
+            // gvbtnDel
+            // 
+            this.gvbtnDel.HeaderText = "Action";
+            this.gvbtnDel.Name = "gvbtnDel";
+            // 
+            // gvName
+            // 
+            this.gvName.DataPropertyName = "Name";
+            this.gvName.HeaderText = "Name";
+            this.gvName.Name = "gvName";
+            // 
+            // gvGender
+            // 
+            this.gvGender.DataPropertyName = "Gender";
+            this.gvGender.HeaderText = "Gender";
+            this.gvGender.Name = "gvGender";
+            // 
+            // gvMarried
+            // 
+            this.gvMarried.DataPropertyName = "Married";
+            this.gvMarried.HeaderText = "Married";
+            this.gvMarried.Name = "gvMarried";
+            // 
+            // gvBirthday
+            // 
+            this.gvBirthday.DataPropertyName = "Birthday";
+            this.gvBirthday.HeaderText = "Birthday";
+            this.gvBirthday.Name = "gvBirthday";
             // 
             // Form1
             // 
@@ -120,15 +165,15 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.ckMarried);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gvSample);
             this.Name = "Form1";
             this.Text = "DataGridView Usage";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSample)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,15 +181,20 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gvSample;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.CheckBox ckMarried;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gvbtnDel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gvName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gvGender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gvMarried;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gvBirthday;
     }
 }
 
--- DataGridView-winform/Form1.cs
+++ DataGridView-winform/Form1.cs
@@ -10,8 +10,34 @@ using System.Windows.Forms;
 
 namespace DataGridView_winform {
     public partial class Form1 : Form {
+
+        private DataTable sampleData() {
+            using (DataTable table = new DataTable()) {
+                // Add columns.
+                table.Columns.Add("Name", typeof(string));
+                table.Columns.Add("Gender", typeof(string));
+                table.Columns.Add("Married ", typeof(int));
+                table.Columns.Add("Birthday", typeof(DateTime));
+
+                // Add rows.
+                table.Rows.Add("Allen", "Male", 0, DateTime.Now);
+                table.Rows.Add("Kevin", "Male", 1, DateTime.Now);
+                table.Rows.Add("Dean", "Male", 0, DateTime.Today);
+                table.Rows.Add("Jenny", "Female", 1, DateTime.Today);
+                return table;
+            }
+        }
+
+        public DataTable dt;
         public Form1() {
             InitializeComponent();
+            dt = dt ?? sampleData();
+            gvInit();
         }
+
+        private void gvInit() {
+            gvSample.DataSource = dt;
+        }
+
     }
 }
